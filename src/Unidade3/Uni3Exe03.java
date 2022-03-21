ddcf064ccd615695249e77f298a02ff12bba3334
Mateus-Frnds <mateus.frnds.dev@gmail.com>
2022-03-21 20:07:16 -0300

    add da att 03

---
+++ src/Unidade3/Uni3Exe03.java
@@ -0,0 +1,21 @@
+package Unidade3;
+import java.util.Scanner;
+
+public class Uni3Exe03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Insira o preço do litro da gasolina:");
+        float gasolinalitro = scanner.nextFloat();
+
+        System.out.println("Insira o valor disponível para o pagamento:");
+        float orcamento = scanner.nextFloat();
+
+        double valor = orcamento / gasolinalitro;
+
+        System.out.println("Você consegue abastecer " + valor + " litros de gasolina");
+     
+        scanner.close();
+
+    }
+}
