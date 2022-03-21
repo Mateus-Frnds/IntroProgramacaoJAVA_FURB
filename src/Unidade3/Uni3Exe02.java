1f9bbb22344d38ca9e153abd8e871182a456887e
Mateus-Frnds <mateus.frnds.dev@gmail.com>
2022-03-21 19:48:47 -0300

    add da att 01

---
+++ src/Unidade3/Uni3Exe02.java
@@ -0,0 +1,23 @@
+package Unidade3;
+import java.util.Scanner;
+
+public class Uni3Exe02 {
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        float valor, desconto, preco;
+
+        System.out.println("Informe o valor do sapato: ");
+        valor = scanner.nextFloat();
+
+        desconto = (float) (valor * 0.12);
+        preco = valor - desconto;
+
+        System.out.printf("O valor do desconto é de R$%.2f%n", desconto);
+        System.out.print("");
+        System.out.printf("O preço do par de sapatos com desconto é R$%.2f%n", preco);
+
+        scanner.close();
+
+    }
+}
