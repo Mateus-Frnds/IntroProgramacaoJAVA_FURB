d160d5b7e8b5c7bc07ba18e5f55b268bc99bce9b
Mateus-Frnds <mateus.frnds.dev@gmail.com>
2022-03-21 15:58:32 -0300

    adicao da att 05 da lista

---
+++ src/Unidade2/Uni2Exe05.java
@@ -0,0 +1,28 @@
+package Unidade2;
+import java.util.Scanner;
+
+public class Uni2Exe05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a, b, c, d, diferenca;
+
+        System.out.println("Insira o primeiro número:");
+        a = scanner.nextInt();
+
+        System.out.println("Insira o segundo número:");
+        b = scanner.nextInt();
+
+        System.out.println("Insira o terceiro número:");
+        c = scanner.nextInt();
+
+        System.out.println("Insira o quarto número:");
+        d = scanner.nextInt();
+
+        diferenca = (a * b) - (c * d);
+
+        System.out.println("A diferença entre os produtos dos valores 1 e 2 por 3 e 4 é: " + diferenca);
+        
+        scanner.close();
+    }
+}
