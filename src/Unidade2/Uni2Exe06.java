aa1ee20223fcb1b75ec8c4903da27c6a80126785
Mateus-Frnds <mateus.frnds.dev@gmail.com>
2022-03-21 16:46:12 -0300

    add da att 06

--- src/Unidade2/Uni2Exe06.java
+++ src/Unidade2/Uni2Exe06.java
@@ -1,8 +1,28 @@
 package Unidade2;
+import java.util.Scanner;
 
 public class Uni2Exe06 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        String nfuncionario;
+        int horas;
+        double salario, soma;
+
+        System.out.println("Insira o número correspondente ao funcionário:");
+        nfuncionario = scanner.next();
+
+        System.out.println("Insira o número de horas trabalhadas do mesmo funcionário:");
+        horas = scanner.nextInt();
+
+        System.out.println("Insira o salário do funcionário por hora trabalhada:");
+        salario = scanner.nextDouble();
+
+        soma = horas * salario;
+        System.out.printf("O total do salário do funcionário " + nfuncionario + " é de R$%.2f%n", soma);
+
+        scanner.close();
+
     }
     
 }
