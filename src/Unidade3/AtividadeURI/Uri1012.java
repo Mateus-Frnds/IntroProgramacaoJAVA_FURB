9e8a1b1794ab65635132fe91cc019788f2aca8da
Mateus-Frnds <mateus.frnds.dev@gmail.com>
2022-03-22 19:59:16 -0300

    add da att URI1012 feita pela metade

---
+++ src/Unidade3/AtividadeURI/Uri1012.java
@@ -0,0 +1,16 @@
+package Unidade3.AtividadeURI;
+import java.util.Scanner;
+
+public class Uri1012 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double acateto, bcateto, chipotenusa, triangulo, circulo, trapezio,
+        quadrado, retangulo;
+
+        acateto = scanner.nextDouble();
+        bcateto = scanner.nextDouble();
+        chipotenusa = scanner.nextDouble();
+
+    }
+}
