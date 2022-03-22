f307032fb9f52852dd1f523e800df7df4692a594
Mateus-Frnds <mateus.frnds.dev@gmail.com>
2022-03-22 15:45:54 -0300

    add da att Uri1007

---
+++ src/Unidade3/AtividadeURI/Uri1007.java
@@ -0,0 +1,22 @@
+package Unidade3.AtividadeURI;
+import java.util.Scanner;
+
+public class Uri1007 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a, b, c, d, diferenca;
+
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        c = scanner.nextInt();
+        d = scanner.nextInt();
+
+        diferenca = (a * b) - (c * d);
+
+        System.out.println("DIFERENCA = " + diferenca);
+        
+        scanner.close();
+    }
+    
+}
