aaf40e8fc647988d146f8c2310cec2efe37422ad
Mateus-Frnds <mateus.frnds.dev@gmail.com>
2022-03-21 21:34:55 -0300

    add da att 02 do URI

---
+++ src/Unidade3/AtividadeURI/Uri1001.java
@@ -0,0 +1,17 @@
+package Unidade3.AtividadeURI;
+import java.util.Scanner;
+
+public class Uri1001 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        int x = a + b;
+        System.out.println("X = " + x);
+
+        scanner.close();
+    }
+    
+}
