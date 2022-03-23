5a6737ddb3ff30ca4fcdef9ae414ee5a6fa66c4f
Mateus-Frnds <mateus.frnds.dev@gmail.com>
2022-03-23 12:21:57 -0300

    add do URI 1015

--- src/Unidade3/AtividadeURI/Uri1015.java
+++ src/Unidade3/AtividadeURI/Uri1015.java
@@ -5,16 +5,20 @@ public class Uri1015 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        float x1, y1, x2, y2;
+        float x1, y1, x2, y2, total;
+        double resultado;
 
         x1 = scanner.nextFloat();
         y1 = scanner.nextFloat();
         x2 = scanner.nextFloat();
         y2 = scanner.nextFloat();
 
-        
+        total =  (((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+        resultado = Math.sqrt(total);
 
+        System.out.printf("%.4f%n", resultado);
 
+        scanner.close();
     
     }
     
