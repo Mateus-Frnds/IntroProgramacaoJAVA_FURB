036a37cc55ab8e4c96887724041e4c1636197076
Mateus-Frnds <mateus.frnds.dev@gmail.com>
2022-03-22 21:35:20 -0300

    add das att do URI mais a conta do imc da barbara

---
+++ src/Unidade3/AtividadeURI/Uri1015.java
@@ -0,0 +1,21 @@
+package Unidade3.AtividadeURI;
+import java.util.Scanner;
+
+public class Uri1015 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        float x1, y1, x2, y2;
+
+        x1 = scanner.nextFloat();
+        y1 = scanner.nextFloat();
+        x2 = scanner.nextFloat();
+        y2 = scanner.nextFloat();
+
+        
+
+
+    
+    }
+    
+}
