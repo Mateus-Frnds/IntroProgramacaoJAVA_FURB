958d045ef85dd189ff60ccda1e99cb76daede4e3
Mateus-Frnds <mateus.frnds.dev@gmail.com>
2022-03-23 18:59:14 -0300

    add da att URI1010

--- src/Unidade3/AtividadeURI/Uri1016.java
+++ src/Unidade3/AtividadeURI/Uri1016.java
@@ -1,1 +1,16 @@
+package Unidade3.AtividadeURI;
+import java.util.Scanner;
 
+public class Uri1016 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int km = scanner.nextInt();
+        int minutos = (60 * km) / 30;
+        System.out.println(minutos + " minutos"); 
+
+        scanner.close();
+
+    }
+    
+}
