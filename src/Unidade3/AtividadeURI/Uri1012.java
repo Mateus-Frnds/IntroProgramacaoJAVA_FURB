e911f1de86cf7376e69537d60a432773be8144c5
Mateus-Frnds <mateus.frnds.dev@gmail.com>
2022-03-22 21:10:10 -0300

    add da att URI1015

--- src/Unidade3/AtividadeURI/Uri1012.java
+++ src/Unidade3/AtividadeURI/Uri1012.java
@@ -5,12 +5,27 @@ public class Uri1012 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        double acateto, bcateto, chipotenusa, triangulo, circulo, trapezio,
+        double a, b, c, triangulo, circulo, trapezio,
         quadrado, retangulo;
 
-        acateto = scanner.nextDouble();
-        bcateto = scanner.nextDouble();
-        chipotenusa = scanner.nextDouble();
+        a = scanner.nextDouble();
+        b = scanner.nextDouble();
+        c = scanner.nextDouble();
+
+        //a = base, c altura, b hipotenusa;
+        triangulo = (a * c) / 2;
+        circulo = (c * c) * 3.14159;
+        trapezio = ((a + b) * c) / 2;
+        quadrado = b * b;
+        retangulo = a * b;
+
+        System.out.printf("TRIANGULO: %.3f%n", triangulo);
+        System.out.printf("CIRCULO: %.3f%n", circulo);
+        System.out.printf("TRAPEZIO: %.3f%n", trapezio);
+        System.out.printf("QUADRADO: %.3f%n", quadrado);
+        System.out.printf("RETANGULO: %.3f%n", retangulo);
+
+        scanner.close();
 
     }
 }
